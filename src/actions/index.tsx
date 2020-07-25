78da4ba6480557ee4325ed397c41c59c4688579c
Andrey <spiridonov.new@gmail.com>
2020-07-25 15:47:51 +0300

    add templates actions

---
+++ src/actions/index.tsx
@@ -0,0 +1,21 @@
+import { ITableItem } from "../models/TableItem.model";
+
+export const loadSmallData = () => async (dispatch: React.Dispatch<any>) => {};
+export const loadBigData = () => async (dispatch: React.Dispatch<any>) => {};
+
+export const loadDataAction = () =>
+  ({
+    type: "LOAD_DATA",
+  } as const);
+
+export const loadDataSuccessAction = (items: ITableItem[]) =>
+  ({
+    type: "LOAD_DATA_SUCCESSED",
+    payload: { items },
+  } as const);
+
+export const loadDataFailedAction = (error: string) =>
+  ({
+    type: "LOAD_DATA_FAILED",
+    payload: { error },
+  } as const);
