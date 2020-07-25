cf643831751b98d84dc2188846978afa55831a92
Andrey <spiridonov.new@gmail.com>
2020-07-25 15:40:39 +0300

    create table-item model

---
+++ src/models/TableItem.model.tsx
@@ -0,0 +1,14 @@
+export interface ITableItem {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  address: {
+    streetAddress: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+  description: string;
+}
